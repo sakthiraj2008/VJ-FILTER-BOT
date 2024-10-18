a4138ebbf2ffe3f3c23c58953ea54fba53bd236b
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-10-18 17:13:36 +0530

    latest

--- plugins/commands.py
+++ plugins/commands.py
@@ -383,10 +383,10 @@ async def start(client, message):
         chat_id = temp.SHORT.get(user)
         settings = await get_settings(chat_id)
         files_ = await get_file_details(file_id)
-        files = files_[0] 
-        title = ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+        files = files_[0]
+        title = ' '.join([x for x in files.file_name.split() if not x.startswith('[') and not x.startswith('@') and x not in ['mkv', 'mp4']])
         g = await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=file_{file_id}")
-        k = await client.send_message(chat_id=user,text=f"<b>📂 Fɪʟᴇ Nᴀᴍᴇ :</b> <i>{title}</i> \n\n<b>⚙️ Fɪʟᴇ Sɪᴢᴇ :</b> <i>{get_size(files.file_size)}</i>\n\n<b>🔗 Fɪʟᴇ Lɪɴᴋ :</b> <i>{g}</i>\n\n<b><i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
+        k = await client.send_message(chat_id=user,text=f"<b>📂 Fɪʟᴇ Nᴀᴍᴇ : {title} \n\n⚙️ Fɪʟᴇ Sɪᴢᴇ : {get_size(files.file_size)}\n\n🔗 Fɪʟᴇ Lɪɴᴋ : {g}\n\n<i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton('📂 Dᴏᴡɴʟᴏᴀᴅ Nᴏᴡ 📂', url=g)
@@ -476,7 +476,7 @@ async def start(client, message):
             files = files_[0]
             title = ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
             g = await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=file_{file_id}")
-            k = await client.send_message(chat_id=message.from_user.id,text=f"<b>📂 Fɪʟᴇ Nᴀᴍᴇ :</b> <i>{title}</i> \n\n<b>⚙️ Fɪʟᴇ Sɪᴢᴇ :</b> <i>{get_size(files.file_size)}</i>\n\n<b>🔗 Fɪʟᴇ Lɪɴᴋ :</b> <i>{g}</i>\n\n<b><i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
+            k = await client.send_message(chat_id=message.from_user.id,text=f"<b>📂 Fɪʟᴇ Nᴀᴍᴇ : {title} \n\n⚙️ Fɪʟᴇ Sɪᴢᴇ : {get_size(files.file_size)}\n\n🔗 Fɪʟᴇ Lɪɴᴋ : {g}\n\n<i>Note: This message is deleted in 20 mins to avoid copyrights. Save the link to Somewhere else</i></b>", reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton('📂 Dᴏᴡɴʟᴏᴀᴅ Nᴏᴡ 📂', url=g)
